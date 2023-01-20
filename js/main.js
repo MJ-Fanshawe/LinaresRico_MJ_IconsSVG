df012906465e4f5346867454faadb861fe0b8415
MJ - Fanshawe <m_linaresrico@fanshaweonline.ca>
2023-01-20 11:22:51 -0500

    week 3 class

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ quarySelector is the method (fuction) that makes the connection bertween JS and 
 
 
 // 1- Make the connection
-const jpgGraphic = document.querySelector ("#bitmap");
+// const jpgGraphic = document.querySelector ("#bitmap");
 
-const vectorGraphic = document.querySelector ("#vector");
+const vectorGraphic = document.querySelector ("#newBadge");
 
 function logID() {
     console.log('clicked on this element:', this.id);
@@ -28,7 +28,7 @@ function logID() {
 // 2 - decide how you want the user interact with the object you created in step 1
 
 // Action after "click"
-jpgGraphic.addEventListener("click", logID);
-vectorGraphic.addEventListener("click", logID);
+// jpgGraphic.addEventListener("click", logID);
+// vectorGraphic.addEventListener("click", logID);
 
 
